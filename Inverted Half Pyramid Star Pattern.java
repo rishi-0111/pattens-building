1c7618727bb13a0ff24af61ace4959c89e96c7c9
RESHI ARASU D <158203661+rishi-0111@users.noreply.github.com>
2026-07-30 14:46:05 +0530

    Create Inverted Half Pyramid Star Pattern.java

---
+++ Inverted Half Pyramid Star Pattern.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Ex {
+    public void start(int n) {
+        for (int i = 0; i <n; i++) {
+            for (int j = i; j <n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        Ex h = new Ex();
+        h.start(n);
+
+        sc.close();
+    }
+}
+
+-----------------------------------------------
+/*
+Sample Input
+5
+Your Output
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+*/
