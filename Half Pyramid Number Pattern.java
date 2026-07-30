935bfa9942343c52764dd0973ec10c274b03baeb
RESHI ARASU D <158203661+rishi-0111@users.noreply.github.com>
2026-07-30 14:36:18 +0530

    Create Half Pyramid Number Pattern.java

---
+++ Half Pyramid Number Pattern.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Ex {
+    public void start(int n) {
+        for (int i = 0; i <=n; i++) {
+            for (int j = 1; j <=i; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        Ex h = new Ex();
+        h.start(n);
+
+        sc.close();
+    }
+}
+
+-----------------------------------------------------------------
+  /*
+  Sample Input
+5
+Your Output
+
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 
+  */
