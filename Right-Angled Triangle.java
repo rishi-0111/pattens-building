4537f10ed96eee18b2dca4e7468ad2b1fd4907ab
RESHI ARASU D <158203661+rishi-0111@users.noreply.github.com>
2026-07-30 14:26:35 +0530

    Create Right-Angled Triangle.java

---
+++ Right-Angled Triangle.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Ex {
+    public void start(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        Ex h = new Ex();
+        h.start(n);
+
+        sc.close();
+    }
+}
+---------------------------------------
+ /* Sample Input
+5
+Your Output
+* 
+* * 
+* * * 
+* * * * 
+* * * * * */
